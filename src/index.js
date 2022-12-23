1485fb872fe7aee0dd9e18ac8ad575df4adc9ad8
Marcelo Mechi <marcedumechi@gmail.com>
2022-12-23 16:57:01 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-hello
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const element = document.querySelector('#root');
+const root = ReactDOM.createRoot(element);
+
+root.render(<App />);
